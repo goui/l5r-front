663cee1705e35a7b145b29e0d7f0ab1866dda3e1
goui <guillaume.galliano@gmail.com>
2016-12-17 08:53:46 -0500

    Created android project

---
+++ app/src/main/java/fr/l5r/l5r/MainActivity.java
@@ -0,0 +1,12 @@
+package fr.l5r.l5r;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
